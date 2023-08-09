1b110af24fcfeec609e5727e61abc7b8af2bd3a6
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-09 23:33:53 +0100

    📁 Initial commit.

---
+++ src/main/java/net/slqmy/tss_slimebot/TSSSlimeBotPlugin.java
@@ -0,0 +1,12 @@
+package net.slqmy.tss_slimebot;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TSSSlimeBotPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        
+
+    }
+}
