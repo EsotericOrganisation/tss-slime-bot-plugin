fc96fda6d6a5f58cc6b6e041b70aa22d0ff38243
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2023-08-10 00:46:40 +0100

    📄 Switched to gradle kotlin and added JDA.

---
+++ build.gradle.kts
@@ -0,0 +1,67 @@
+plugins {
+    java
+    application
+
+    id("io.papermc.paperweight.userdev") version "1.5.5"
+    id("xyz.jpenilla.run-paper") version "2.1.0"
+}
+
+group = "net.slqmy"
+version = "0.1"
+description = "The plugin to run Slime Bot and synchronise Minecraft server data to the Discord server."
+
+java {
+    toolchain.languageVersion.set(JavaLanguageVersion.of(17))
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    compileOnly(files("../TSS-Core/build/libs/tss_core-0.1-dev-all.jar"))
+
+    paperweight.paperDevBundle("1.20.1-R0.1-SNAPSHOT")
+
+    implementation("net.dv8tion", "JDA", "5.0.0-beta.13")
+}
+
+tasks {
+    assemble {
+        dependsOn(reobfJar)
+    }
+
+    compileJava {
+        options.encoding = Charsets.UTF_8.name()
+        options.release.set(17)
+    }
+
+    javadoc {
+        options.encoding = Charsets.UTF_8.name()
+    }
+
+    processResources {
+        filteringCharset = Charsets.UTF_8.name()
+
+        val props = mapOf(
+                "name" to project.name,
+                "version" to project.version,
+                "description" to project.description,
+                "apiVersion" to "1.20"
+        )
+
+        inputs.properties(props)
+
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
+
+    runServer {
+        minecraftVersion("1.20.1")
+    }
+}
+
+application {
+    mainClass.set("TSSSlimeBotPlugin")
+}
