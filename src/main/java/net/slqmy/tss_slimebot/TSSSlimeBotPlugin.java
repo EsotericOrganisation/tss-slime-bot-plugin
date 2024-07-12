4352f57665d6e33caf80b432a12c169bb8a34314
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-07-12 02:55:48 +0100

    Fix imports

--- src/main/java/net/slqmy/tss_slimebot/TSSSlimeBotPlugin.java
+++ src/main/java/net/slqmy/tss_slimebot/TSSSlimeBotPlugin.java
@@ -1,8 +1,8 @@
 package net.slqmy.tss_slimebot;
 
-import net.slqmy.tss_core.shaded.net.dv8tion.jda.api.JDA;
-import net.slqmy.tss_core.shaded.net.dv8tion.jda.api.JDABuilder;
-import net.slqmy.tss_core.shaded.net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.JDA;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
 import net.slqmy.tss_core.util.DebugUtil;
 import net.slqmy.tss_slimebot.event.listener.ReadyListener;
 import org.bukkit.configuration.file.YamlConfiguration;
--- src/main/java/net/slqmy/tss_slimebot/event/listener/ReadyListener.java
+++ src/main/java/net/slqmy/tss_slimebot/event/listener/ReadyListener.java
@@ -1,8 +1,8 @@
 package net.slqmy.tss_slimebot.event.listener;
 
-import net.slqmy.tss_core.shaded.net.dv8tion.jda.api.events.GenericEvent;
-import net.slqmy.tss_core.shaded.net.dv8tion.jda.api.events.session.ReadyEvent;
-import net.slqmy.tss_core.shaded.net.dv8tion.jda.api.hooks.EventListener;
+import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.session.ReadyEvent;
+import net.dv8tion.jda.api.hooks.EventListener;
 import net.slqmy.tss_core.util.LogUtil;
 import org.jetbrains.annotations.NotNull;
 
